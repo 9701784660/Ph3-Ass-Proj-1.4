8d87b6f4108ba8155bc0b3e6745a2468844783f0
praveen4858 <praveenryali048@gmail.com>
2023-08-27 10:31:07 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _1._4__using_controllers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
